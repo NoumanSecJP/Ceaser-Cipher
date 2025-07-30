2b3324b8028efbfe6a4847142515852e121219cf
Syed Noman Uddin <184674305+NoumanSecJP@users.noreply.github.com>
2025-07-30 11:11:16 +0500

    Updated Caesar Cipher code with cleaner logic and bug fixes

--- ceaser cipher.py
+++ ceaser cipher.py
@@ -2,7 +2,7 @@ while True:
  choice=input("\n1 for Encrypt / 0 for Decrypt:  ")     
  alphabets=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
  if choice=="1":
-   user=input("Enter the plain text to convert into ceaser cipher: ")
+   user=input("Enter the plain text to convert into ceaser cipher: ").lower()
    result=""
    key=int(input("Enter your key: "))
    for char in user:
@@ -11,10 +11,10 @@ while True:
         add=(a+key)%26
         alpha=(alphabets[add])
         result+=alpha
-        print(alpha,end="")
+   print(result)
         
  elif choice=="0":
-     user=input("Enter your decrypted text: ")
+     user=input("Enter your decrypted text: ").lower()
      result=""
      key=int(input("Enter your key: "))
      for char in user:
@@ -23,7 +23,7 @@ while True:
         add=(a-key)%26
         alpha=(alphabets[add])
         result+=alpha
-        print(alpha,end="")
+     print(result)
 
  else:
      print("Kindly enter only one word (alphabets) in small letters ")
